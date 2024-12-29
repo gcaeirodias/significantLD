749c7b5cb2748efe296fed738b3ed707782f89f7
Guilherme Caeiro-Dias <53879538+gcaeirodias@users.noreply.github.com>
2024-12-29 10:06:27 -0700

    Update LDpairs.R

--- LDpairs.R
+++ LDpairs.R
@@ -1,11 +1,10 @@
 library(dplyr)
-library(stringr)
 
 # Function to load all files with significant LD loci from all populations (from multi_corr_LD.R).
 load_ld_files <- function(suffix = "*_signLD_loci.txt") {
-  ld_files <- list.files(suffix = suffix, recursive = FALSE)
+  ld_files <- list.files(pattern = suffix, recursive = FALSE)
   if (length(ld_files) == 0) {
-    stop("No files found with the pattern:", pattern)
+    stop("No files found with the suffix:", suffix)
   }
   ld_data <- lapply(ld_files, function(file) {
     tryCatch(
@@ -52,32 +51,25 @@ filter_ld_pairs_by_pop <- function(ld_pairs_counts, n_pops) {
 }
 
 # Function to save results.
-save_ld_files <- function(ld_pairs_counts, ld_pairs_across_pops, ld_pairs_across_pops_sorted) {
-  write.table(ld_pairs_counts, file = "ld_pairs_counts.txt", quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
-  write.table(ld_pairs_across_pops, file = "ld_pairs.txt", quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
+save_ld_files <- function(ld_pairs_across_pops_sorted) {
   write.table(ld_pairs_across_pops_sorted, file = "significant_ld_loci.txt", quote = FALSE, sep = "\t", col.names = TRUE, row.names = FALSE)
 }
 
-# Function to run all previous functions sequencially.
-LD_pairs <- function(suffix = "*_signLD_loci.txt", n_pops = 2) {
+# Function to run all previous functions sequentially.
+LDpairs <- function(suffix = "*_signLD_loci.txt", n_pops = 2) {
   
   # Step 1: Load files with with significant LD loci.
-  ld_data <- load_ld_files(pattern)
-  
+  ld_data <- load_ld_files(suffix)
   # Step 2: Combine significant LD loci from all populations.
   ld_pairs_sorted <- combine_ld_loci(ld_data)
-  
   # Step 3: Count occurrences of each unique LD pair.
   ld_pairs_counts <- count_ld_pairs(ld_pairs_sorted)
-  
   # Step 4: Filter pairs that are found in at least 'n_pops' populations.
   ld_pairs_across_pops <- filter_ld_pairs_by_pop(ld_pairs_counts, n_pops)
-  
   # Step 5: Sort the filtered LD pairs for final output.
   ld_pairs_across_pops_sorted <- ld_pairs_across_pops[order(ld_pairs_across_pops$CHROM_SNP1, ld_pairs_across_pops$CHROM_SNP2, ld_pairs_across_pops$POS_SNP1, ld_pairs_across_pops$POS_SNP2), ]
-  
   # Step 6: Save the results.
-  save_ld_files(ld_pairs_counts, ld_pairs_across_pops, ld_pairs_across_pops_sorted)
+  save_ld_files(ld_pairs_across_pops_sorted)
   
   # Return the results in case further analysis is needed.
   return(list(
